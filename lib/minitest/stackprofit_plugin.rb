e1aef9030709f37a4583778f6bb8c377740b1d4a
Sylvain <sylvain.joyeux@tidewise.io>
2024-08-29 13:33:40 -0300

    add the --stackprof-raw option
    'raw' output allows to generate reports such as flamegraphs.

--- lib/minitest/stackprofit_plugin.rb
+++ lib/minitest/stackprofit_plugin.rb
@@ -7,6 +7,7 @@ module Minitest
 
   @stackprof_path = nil
   @stackprof_type = :wall
+  @stackprof_raw = false
 
   def self.plugin_stackprofit_options opts, options # :nodoc:
     opts.on "--stackprof [path]", String, "Save profiling to [path]." do |s|
@@ -16,13 +17,17 @@ module Minitest
     opts.on "--stackprof-type type", String, "Set profile type (default: wall)." do |s|
       @stackprof_type = s.to_sym
     end
+
+    opts.on "--stackprof-raw", "Save raw samples (for flamegraph generation)" do |s|
+      @stackprof_raw = true
+    end
   end
 
   def self.plugin_stackprofit_init options # :nodoc:
     if @stackprof_path then
       require "stackprof"
 
-      StackProf.start mode: @stackprof_type, out: @stackprof_path
+      StackProf.start mode: @stackprof_type, out: @stackprof_path, raw: @stackprof_raw
 
       Minitest.after_run do
         StackProf.stop
